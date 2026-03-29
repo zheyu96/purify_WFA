6c6d5b691a3042e843dc4b97ccad80118b2f6daf
zheyu96 <zheyu@gmail.com>
2026-03-30 03:06:07 +0800

    purify_first

--- src/main.cpp
+++ src/main.cpp
@@ -283,7 +283,7 @@ int main(){
 
 
                     double A = 0.25, B = 0.75, tao = input_parameter["tao"], T = 0.04, n = 2;
-                    Graph graph(filename, time_limit, swap_prob, avg_memory, min_fidelity, max_fidelity, fidelity_threshold, A, B, n, T, tao,Zmin,bucket_eps,time_eta);
+                    Graph graph(filename, time_limit, swap_prob, avg_memory, min_fidelity, max_fidelity, fidelity_threshold, A, B, n, T, tao,Zmin,bucket_eps,time_eta,input_parameter["delta_P"]);
 
                     ofs << "--------------- in round " << r << " -------------" <<endl;
                     vector<pair<int, int>> requests;
