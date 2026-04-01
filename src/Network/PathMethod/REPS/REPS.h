2bdaceb330b689f174f199d995bcc21edcc48210
zheyu96 <zheyu@gmail.com>
2026-04-01 21:39:46 +0800

    purify_first

--- src/Network/PathMethod/REPS/REPS.h
+++ src/Network/PathMethod/REPS/REPS.h
@@ -1,12 +1,8 @@
 #include "../PathMethodBase/PathMethod.h"
-// #include "gurobi_c++.h"  // disabled: Gurobi not available on this system
 
 using namespace std;
 
 class REPS : public PathMethod {
-    void PFT_LP(vector<double> &t_plum, vector<map<pair<int, int>, double>> &f_plum);
-    pair<Path, double> dijkstra(int src, int dst, map<pair<int, int>, double>&f_plum_i);
-    void update_f_plum(Path path, double width, map<pair<int, int>, double>&f_plum_i);
 public:
     REPS();
     ~REPS();
